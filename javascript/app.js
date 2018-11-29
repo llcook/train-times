b71bec3d0e7da94f4f8020f2e7c8d422c1a5d52d
watercolorhorse <29869241+llcook@users.noreply.github.com>
2018-11-28 23:47:01 -0700

    Cleaned up JavaScript document and added comments

--- javascript/app.js
+++ javascript/app.js
@@ -1,29 +1,29 @@
-// Initialize Firebase
+// INITIALIZE DATABASE
 var config = {
     apiKey: "AIzaSyAXCM4xF-y-Z_RThOBgEIHr9Pii1b4D39Y",
     authDomain: "fir-558d2.firebaseapp.com",
     databaseURL: "https://fir-558d2.firebaseio.com",
-    // projectId: "fir-558d2",
+    projectId: "fir-558d2",
     storageBucket: "fir-558d2.appspot.com",
-    // messagingSenderId: "497309884882"
+    messagingSenderId: "497309884882"
 };
 firebase.initializeApp(config);
 
 var database = firebase.database();
 
 // BUTTON FOR ADDING TRAINS
-$("#submit").on("click", function(event){
-    
-    //Prevent the submit button from reloading the page.
+$("#submit").on("click", function (event) {
+
+    // PREVENT SUBMIT BUTTON FROM RELOADING PAGE
     event.preventDefault();
 
-    //Get the values from the submission form.
+    // GRAB VALUES FROM SUBMISSION FORM
     var trainName = $("#trainName").val().trim();
     var destination = $("#destination").val().trim();
     var firstTrain = $("#firstTrain").val().trim();
     var frequency = $("#frequency").val().trim();
 
-    //Push the values to the database.
+    // PUSH VALUES TO DATABASE
     database.ref().push({
         trainName: trainName,
         destination: destination,
@@ -34,18 +34,16 @@ $("#submit").on("click", function(event){
 
 // FIREBASE EVENT FOR ADDING TRAIN TO DATABASE
 // AND ADD ROW WHEN USER ADDS ENTRY
+database.ref().on("child_added", function (childSnapshot) {
 
-database.ref().on("child_added", function(childSnapshot) {
-
-    console.log(childSnapshot.val());
-
+    // STORE CHILD DATA
     trainName = childSnapshot.val().trainName;
     destination = childSnapshot.val().destination;
     firstTrain = moment((childSnapshot.val().firstTrain), "HHmm").subtract(1, "days");
     frequency = childSnapshot.val().frequency;
-    
+
+    // INTEGRATE MOMENT.JS AND CALCULATE TIMES
     var currentTime = moment();
-    console.log(moment(currentTime).format("HHmm"));
 
     var timeDiff = currentTime.diff(moment(firstTrain), "minutes");
     var timeLeft = timeDiff % frequency;
@@ -54,19 +52,9 @@ database.ref().on("child_added", function(childSnapshot) {
     var nextTrain = moment().add(minsAway, "minutes");
     var nextArr = moment(nextTrain).format("hh:mm a");
 
-
+    // APPEND ROW TO TABLE
     var newRow = $("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArr + "</td><td>" + minsAway + "</td>");
 
     $("tbody").append(newRow);
 
-    console.log(minsAway);
-
 });
-
-////////////////////////////////////////////////
-
-// ON INITIAL LOAD, GET SNAPSHOT OF STORED DATA
-// FUNCTION ALLOWS YOU TO UPDATE PAGE IN REAL-TIME WHEN FBDB CHANGES
-// see codersbay activity for reference
-
-////////////////////////////////////////////////
